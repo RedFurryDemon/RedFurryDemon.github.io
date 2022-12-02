f47beee916e878f1309461bb244e20b6150b38cd
RedFurryDemon <55105171+RedFurryDemon@users.noreply.github.com>
2022-12-02 14:34:03 +0100

    favicon & phylum scripting

--- scripts/phylum.js
+++ scripts/phylum.js
@@ -1,24 +1,41 @@
 //stuff for phylogenetic trees
 
-//fold/show clades
+const descriptions = document.querySelectorAll(".clade_desc");
+const clades = document.querySelectorAll(".clade, .species");
 
+document.getElementById("b_toggleDesc").addEventListener("click", toggleDescriptions);
+document.getElementById("b_toggleIndent").addEventListener("click", toggleIndentation);
+
+//fold/show clades
 var toggler = document.getElementsByClassName("clade_name");
 var i;
 
 for (i = 0; i < toggler.length; i++) {
-	toggler[i].addEventListener("click", toggleSubclades());
+	toggler[i].addEventListener("click", function () {
+		this.parentElement.querySelector(".clade_desc").classList.toggle("clade_desc_hidden");
+		this.classList.toggle("clade_name_hidden");
+		//https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+		var subclades = this.parentElement.querySelectorAll(".clade, .species");
+
+		subclades.forEach((userItem) => {
+			userItem.classList.toggle("hidden");
+		});
+	});
 }
 
-function toggleSubclades() {
-	this.classList.toggle("clade_name_hidden");
-	this.parentElement.querySelector(".clade_desc").classList.toggle("clade_desc_hidden");
+//toggle descriptions
+function toggleDescriptions() {
+	descriptions.forEach((description) => {
+		description.classList.toggle("hidden");
+	});
+	clades.forEach((clade) => {
+		clade.classList.toggle("clade_name_spacing");
+	});
+}
 
-	var subclades = this.parentElement.querySelectorAll("div");
-	subclades.forEach(
-		element => element.classList.toggle("hidden")
-	);
-	var species = this.parentElement.querySelectorAll(".species");
-	species.forEach(
-		element => element.classList.toggle("hidden")
-	);
+//toggle indentation
+function toggleIndentation() {
+	clades.forEach((clade) => {
+		clade.classList.toggle("clade_no_indent");
+	});
 }
